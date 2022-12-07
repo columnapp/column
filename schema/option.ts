04f7e0b7b371e94cab1b873d5a16d30e36cd27e4
Wahyu <wahyudinata@gmail.com>
2022-12-07 00:40:38 -0800

    add value param into form

--- schema/option.ts
+++ schema/option.ts
@@ -35,7 +35,7 @@ function makeConfigItemColumnSchema<Value extends ZodType, ConfigType extends Zo
 }
 export function makeFilterSchema<V extends ZodType>(cellValueSchema: V) {
   // filter input has specific behavior so we need to separate them from say input rendered in cells
-  const form = makeFunctionWithAPIColumn(cellValueSchema, DisplayFilterSchema)
+  const form = makeFunctionWithAPIColumn(cellValueSchema, DisplayFilterSchema, z.any())
   return z
     .discriminatedUnion('type', [
       makeConfigItemColumnSchema(cellValueSchema, ...ValueSchemaMapping['boolean']),
